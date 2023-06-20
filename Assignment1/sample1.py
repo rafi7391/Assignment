ed1bda90debeca4a90cbd9b93c8ce78b84e5b3c7
Shaik Rafi <110666006+rafi7391@users.noreply.github.com>
2023-06-20 23:32:17 +0530

    sample1.py

--- Assignment1/sample1.py
+++ Assignment1/sample1.py
@@ -1,1 +1,21 @@
-
+import re
+import PyPDF2
+# Open the PDF file
+pdfFile = open('sample1.pdf', 'rb')
+# Create a PDF reader object
+pdfReader = PyPDF2.PdfReader(pdfFile)
+# Get the number of pages in the PDF
+numPages = len(pdfReader.pages)
+# Loop over the pages in the PDF
+for i in range(numPages):
+    # Get the page object
+    page = pdfReader.pages[i]
+    # Get the text from the page
+    text = page.extract_text()
+    # Find all the key-value pairs in the text
+    keyValuePairs = re.findall(r'(?P<key>[^=]+)=(?P<value>.+)$', text)
+    # Print the key-value pairs
+    for key, value in keyValuePairs:
+        print(f'{key}: {value}')
+# Close the PDF file
+pdfFile.close()
